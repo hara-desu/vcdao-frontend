3f69bc824fa5bb876c156ea5d65488cd0516dcff
hara-desu <vladawinter33@gmail.com>
2024-04-16 00:20:41 +0900

    Added routing and changed navbar

--- vcdao-app/src/App.js
+++ vcdao-app/src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import Navibar from './component/navibar';
 import Proposal from './component/Proposal';
 import Funding from './component/Funding';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Navibar />
-      <div className='proposal-container'>
-        <Proposal />
-        <Proposal type="add" />
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navibar />}>
+            <Route index element={<Proposal />} />
+            <Route path="funding" element={<Funding />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- vcdao-app/src/component/navibar.js
+++ vcdao-app/src/component/navibar.js
@@ -1,17 +1,32 @@
 import react from "react";
+import "../Navbar.css";
 import "../App.css";
 import MetaMask from "../MetaMask";
+import { Outlet, Link } from "react-router-dom";
+
 
 function Navibar(props) {
     return (
-        <div className="navbar">
-            <h1 className="Name">MEMEDAO</h1>
-            <div className="menu">
-                <h2 className="item">Funding</h2>
-                <h2 className="item">Proposal</h2>
-                <h2 className="item">Mint</h2>
-                <MetaMask />
-            </div>
+        <div>
+            <nav>
+                <ul>
+                    <li className="Name">
+                        MEMEDAO
+                    </li>
+                    <li>
+                        <Link to="/">Proposal</Link>
+                    </li>
+                    <li>
+                        <Link to="/funding">Funding</Link>
+                    </li>
+                    <li>
+                        <MetaMask />
+                    </li>
+                </ul>
+            </nav>
+
+            <Outlet />
+
         </div>
     );
 }
